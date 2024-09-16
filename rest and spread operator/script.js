d7d8a4f8ebb22cae183a55d08be9ecb801c43c4b
Din Bandhu Adhikari <adbandhu23@gmail.com>
2024-09-16 14:17:30 +0545

    spread all usuage

--- rest and spread operator/script.js
+++ rest and spread operator/script.js
@@ -4,3 +4,24 @@
 const array = [1, 2, 3, 4, 5];
 const array2 = [...array];
 console.log(array2);
+// merging array
+
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+const combined = [...arr1, ...arr2];
+console.log(combined);
+
+// passing arguments
+
+function sum(a, b, c) {
+  return a + b + c;
+}
+const numbers = [2, 4, 6];
+console.log(sum(...numbers));
+
+//object merging/cloning
+
+const obj1 = { a: 1, b: 2 };
+const obj2 = { b: 3, c: 4 };
+const combinedObj = { ...obj1, ...obj2 };
+console.log(combinedObj);
