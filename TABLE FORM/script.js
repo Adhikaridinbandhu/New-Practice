94ee6e95b9fee3047c781ee453b6ba97fb5fa42c
Din Bandhu Adhikari <adbandhu23@gmail.com>
2024-09-20 14:48:33 +0545

    form data display

--- TABLE FORM/script.js
+++ TABLE FORM/script.js
@@ -26,11 +26,27 @@ formData.addEventListener("submit", function (event) {
     enteredContact: contactElement.value,
   };
   submittedData.push(inputValue);
+  nameElement.value = "";
+  addressElement.value = "";
+  emailElement.value = "";
+  contactElement.value = "";
+  displayData();
 });
 
 function displayData() {
   let html = "";
   submittedData.forEach(function (ele, index) {
-    html += ``;
+    html += `
+        <div class="card-body" id="card-body">
+          <p class="card-text"><strong>Name: </strong>${ele.enteredName}</p>
+          <p class="card-text"><strong>Address: </strong>${ele.enteredAddress}</p>
+          <p class="card-text"><strong>Email: </strong>${ele.enteredEmail}</p>
+          <p class="card-text"><strong>Phone no: </strong>${ele.enteredContact}</p>
+          <div class="btn-wrapper">
+            <a href="#" class="btn btn-primary">Edit Data</a>
+            <a href="#" class="btn btn-danger">Delete</a>
+          </div>
+        </div>`;
   });
+  tableBody.innerHTML = html;
 }
