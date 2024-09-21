e4d15bedf6f0ffe10e6df44358b6d25fff59a766
Din Bandhu Adhikari <adbandhu23@gmail.com>
2024-09-21 10:20:15 +0545

    form display

--- TABLE FORM/script.js
+++ TABLE FORM/script.js
@@ -12,6 +12,9 @@ let contactElement = document.getElementById("exampleInputPhone1");
 
 let btnElement = document.getElementById("btn-submit");
 
+let CardWrapper = document.getElementById("card-wrapper");
+CardWrapper.innerHTML = "";
+
 tableBody.innerHTML = "";
 
 let submittedData = [];
@@ -36,23 +39,24 @@ formData.addEventListener("submit", function (event) {
 function displayData() {
   let html = "";
   submittedData.forEach(function (ele, index) {
-    html += `
-    <div class="card" style="width: 18rem" id="card-body">
+    html += `<div class="card" style="width: 18rem" id="card-body">
         <img
           src="https://images.unsplash.com/photo-1719937206498-b31844530a96?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxMXx8fGVufDB8fHx8fA%3D%3D"
-           class="card-img-top"
+          class="card-img-top"
           alt="image data"
         />
-        <div class="card-body">
+        <div class="card-body" id="card-data">
           <p class="card-text"><strong>Name: </strong>${ele.enteredName}</p>
           <p class="card-text"><strong>Address: </strong>${ele.enteredAddress}</p>
           <p class="card-text"><strong>Email: </strong>${ele.enteredEmail}</p>
-          <p class="card-text"><strong>Phone no: </strong>${ele.enteredContact}</p>
+          <p class="card-text"><strong>Phone no: </strong>${ele.contact}</p>
           <div class="btn-wrapper">
             <a href="#" class="btn btn-primary">Edit Data</a>
             <a href="#" class="btn btn-danger">Delete</a>
           </div>
-        </div>`;
+        </div>
+      </div>`;
   });
-  tableBody.innerHTML = html;
+
+  CardWrapper.innerHTML = html;
 }
